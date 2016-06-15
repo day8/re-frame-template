b36b3e0af19f9f13a23e54de3100bc9e8af070ea
Matthew Jaoudi <gadfly361@gmail.com>
2016-06-15 01:10:01 -0700

    bump clojure / clojurescript version, #25;

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -1,6 +1,6 @@
 (defproject {{ns-name}} "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
                  [reagent "0.5.1"]
                  [re-frame "0.7.0"]{{#re-com?}}
                  [re-com "0.8.3"]{{/re-com?}}{{#routes?}}
