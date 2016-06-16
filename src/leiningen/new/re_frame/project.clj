7acb1527cc3bcc11c837ff66f0c73060183b9731
Matthew Jaoudi <gadfly361@gmail.com>
2016-06-15 19:45:16 -0700

    move cljs-devtools to main dependencies vector to fix bug when creating uberjar;
    - This isn't a great place for a dev dependency, but the accepted alternative
      (env folders) is quite heavy ... not sure which is worse.

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [reagent "0.5.1"]
+                 [binaryage/devtools "0.6.1"]
                  [re-frame "0.7.0"]{{#re-com?}}
                  [re-com "0.8.3"]{{/re-com?}}{{#routes?}}
                  [secretary "1.2.3"]{{/routes?}}{{#garden?}}
@@ -40,7 +41,7 @@
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.6.1"]{{#cider?}}
+   {:dependencies [{{#cider?}}
                    [lein-figwheel "0.5.4-3"]
                    [com.cemerick/piggieback "0.2.1"]{{/cider?}}]
 
