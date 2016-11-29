14d02742e23454336042f5a0fa017196570d50c9
motor4ik <flexsurfer@users.noreply.github.com>
2016-11-29 21:04:15 +0300

    re-frisk option (#39)
    add re-frisk option;

--- src/leiningen/new/re_frame/src/cljs/core.cljs
+++ src/leiningen/new/re_frame/src/cljs/core.cljs
@@ -1,6 +1,7 @@
 (ns {{ns-name}}.core
     (:require [reagent.core :as reagent]
-              [re-frame.core :as re-frame]
+              [re-frame.core :as re-frame]{{#re-frisk?}}
+              [re-frisk.core :refer [enable-re-frisk!]]{{/re-frisk?}}
               [{{ns-name}}.events]
               [{{ns-name}}.subs]{{#routes?}}
               [{{ns-name}}.routes :as routes]{{/routes?}}
@@ -10,7 +11,8 @@
 
 (defn dev-setup []
   (when config/debug?
-    (enable-console-print!)
+    (enable-console-print!){{#re-frisk?}}
+    (enable-re-frisk!){{/re-frisk?}}
     (println "dev mode")))
 
 (defn mount-root []
