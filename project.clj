fb279f3d85d8eb1a0bc4ba6a8831168b6b9da257
Matthew Jaoudi <gadfly361@gmail.com>
2016-12-14 10:04:33 -0800

    bump re-frisk version, #42;

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame/lein-template "0.2.5-1"
+(defproject re-frame/lein-template "0.2.5-2"
   :description "Leiningen template for a Reagent web app that implements the re-frame pattern."
   :url "https://github.com/Day8/re-frame-template"
   :license {:name "MIT"}
