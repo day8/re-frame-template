d2ff09be2bc5029788197dcbbdabaf2e990ad130
Matthew Jaoudi <gadfly361@gmail.com>
2017-03-05 14:57:06 -0800

    bump re-frame, re-com, figwheel;

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -2,10 +2,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0"]
-                 [re-frame "0.9.1"]{{#re-frisk?}}
+                 [re-frame "0.9.2"]{{#re-frisk?}}
                  [re-frisk "0.3.2"]{{/re-frisk?}}{{#re-com?}}
                  [org.clojure/core.async "0.2.391"]
-                 [re-com "1.3.0"]{{/re-com?}}{{#routes?}}
+                 [re-com "2.0.0"]{{/re-com?}}{{#routes?}}
                  [secretary "1.2.3"]{{/routes?}}{{#garden?}}
                  [garden "1.3.2"]
                  [ns-tracker "0.3.0"]{{/garden?}}{{#handler?}}
@@ -44,10 +44,10 @@
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.8.2"]{{#cider?}}
-                   [figwheel-sidecar "0.5.7"]
+                   [figwheel-sidecar "0.5.9"]
                    [com.cemerick/piggieback "0.2.1"]{{/cider?}}]
 
-    :plugins      [[lein-figwheel "0.5.7"]{{#test?}}
+    :plugins      [[lein-figwheel "0.5.9"]{{#test?}}
                    [lein-doo "0.1.7"]{{/test?}}]
     }}
 
