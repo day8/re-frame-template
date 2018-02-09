8391f09efd6bde488c0922ac89104b21d73fa703
Mike Thompson <mike.thompson+github@day8.com.au>
2018-02-09 21:54:16 +1100

    Update Project.clj with latest `re-frame-trace`

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -55,7 +55,7 @@
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.9.4"]{{#trace?}}
-                   [day8.re-frame/trace "0.1.18"]{{/trace?}}{{#cider?}}
+                   [day8.re-frame/trace "0.1.19"]{{/trace?}}{{#cider?}}
                    [figwheel-sidecar "0.5.13"]
                    [com.cemerick/piggieback "0.2.2"]{{/cider?}}{{#re-frisk?}}
                    [re-frisk "0.5.3"]{{/re-frisk?}}]
