1d4633e9eb09ed1106705e4cc00bcfb72a21c3d3
Daniel Compton <desk@danielcompton.net>
2019-03-11 10:36:04 +1300

    Version 0.3.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame/lein-template "0.3.19-SNAPSHOT"
+(defproject re-frame/lein-template "0.3.19"
   :description "Leiningen template for a Reagent web app that implements the re-frame pattern."
   :url "https://github.com/Day8/re-frame-template"
   :license {:name "MIT"}
