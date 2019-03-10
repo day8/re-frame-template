648f6ebea35948ec5d431c0f3d0090032c690bc3
Daniel Compton <desk@danielcompton.net>
2019-03-11 07:30:14 +1300

    Set deploy repositories

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Leiningen template for a Reagent web app that implements the re-frame pattern."
   :url "https://github.com/Day8/re-frame-template"
   :license {:name "MIT"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :scm {:name "git"
         :url "https://github.com/Day8/re-frame-template"}
   :eval-in-leiningen true)
