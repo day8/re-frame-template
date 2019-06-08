f84b33ebc0c991a9b3350c592b97e7e0dbf2233b
Isaac Johnston <isaac@superstruct.nz>
2019-06-08 05:54:08 +0300

    Upgrade Clojure dependency to 1.10.1

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -1,5 +1,5 @@
 (defproject {{ns-name}} "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
                  [re-frame "0.10.6"]{{#re-com?}}
