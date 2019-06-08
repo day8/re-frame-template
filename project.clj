5af556570079b44a6c0200a1f137b54005068d5e
Isaac Johnston <isaac@superstruct.nz>
2019-06-08 15:59:21 +0300

    Change lein release and deploy

--- project.clj
+++ project.clj
@@ -2,8 +2,22 @@
   :description "Leiningen template for a Reagent web app that implements the re-frame pattern."
   :url "https://github.com/Day8/re-frame-template"
   :license {:name "MIT"}
-  :deploy-repositories [["releases" :clojars]
-                        ["snapshots" :clojars]]
+  
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url "https://clojars.org/repo"
+                                    :username :env/CLOJARS_USERNAME
+                                    :password :env/CLOJARS_PASSWORD}]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
   :scm {:name "git"
         :url "https://github.com/Day8/re-frame-template"}
+  
   :eval-in-leiningen true)
