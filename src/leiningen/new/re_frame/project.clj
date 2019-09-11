e602a9bc28cfe2e9db8ed9c3168d6b56703cb2fc
Isaac Johnston <isaac@superstruct.nz>
2019-09-11 19:52:56 +0800

    Upgrade re-frame-10x to 0.4.3

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -49,7 +49,7 @@
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.9.10"]{{#10x?}}
-                   [day8.re-frame/re-frame-10x "0.4.2"]
+                   [day8.re-frame/re-frame-10x "0.4.3"]
                    [day8.re-frame/tracing "0.5.3"]{{/10x?}}{{#re-frisk?}}
                    [re-frisk "0.5.4.1"]{{/re-frisk?}}]}
 
