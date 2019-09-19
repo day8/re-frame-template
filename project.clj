72fa826f9c812b89725f2ebe36b0151fbd677123
Isaac Johnston <isaac@superstruct.nz>
2019-09-20 03:21:22 +0800

    Version 1.0.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame/lein-template "1.0.8"
+(defproject re-frame/lein-template "1.0.9-SNAPSHOT"
   :description "Leiningen template for a Reagent web app that implements the re-frame pattern."
   :url "https://github.com/Day8/re-frame-template"
   :license {:name "MIT"}
