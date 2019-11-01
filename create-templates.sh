b3f2e0ea90b2ac7b1b0312a5b03f436812e13db7
Isaac Johnston <isaac@superstruct.nz>
2019-11-02 00:46:47 +0800

    Migrate to GitHub Actions based ci and cd

--- create-templates.sh
+++ create-templates.sh
@@ -10,7 +10,7 @@ cd base
 npm install
 lein prod
 cd resources/public
-google-chrome index.html
+google-chrome --headless --disable-gpu index.html
 cd ../../..
 
 printf "\ncreating base +routes\n"
@@ -19,7 +19,7 @@ cd base-routes
 npm install
 lein prod
 cd resources/public
-google-chrome index.html
+google-chrome --headless --disable-gpu index.html
 cd ../../..
 
 printf "\ncreating base +re-com\n"
@@ -28,7 +28,7 @@ cd base-recom
 npm install
 lein prod
 cd resources/public
-google-chrome index.html
+google-chrome --headless --disable-gpu index.html
 cd ../../..
 
 printf "\ncreating base +routes +re-com\n"
@@ -37,7 +37,7 @@ cd base-routes-recom
 npm install
 lein prod
 cd resources/public
-google-chrome index.html
+google-chrome --headless --disable-gpu index.html
 cd ../../..
 
 printf "\ncreating base +cider +routes +re-com +test +garden +handler +less +re-frisk +10x +re-pressed +breaking-point +shadow-cljs\n"
@@ -48,6 +48,6 @@ lein prod
 npm install -g karma-cli
 lein run -m shadow.cljs.devtools.cli compile karma-test
 cd resources/public
-google-chrome index.html
+google-chrome --headless --disable-gpu index.html
 
 cd ../../..
