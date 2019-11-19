4abc162a6d017b736c1a71eb13bfb2d9d26534d8
Isaac Johnston <isaac@superstruct.nz>
2019-11-19 13:17:10 +0800

    Migrate to day8/lein-git-inject

--- project.clj
+++ project.clj
@@ -1,25 +1,12 @@
-(defproject re-frame/lein-template "see :git-version below https://github.com/arrdem/lein-git-version"
+(defproject re-frame/lein-template "lein-git-inject/version"
   :description "Leiningen template for a Reagent web app that implements the re-frame pattern."
   :url "https://github.com/day8/re-frame-template"
   :license {:name "MIT"}
 
-  :git-version
-  {:status-to-version
-   (fn [{:keys [tag version branch ahead ahead? dirty?] :as git-status}]
-     (if-not (string? tag)
-       ;; If git-status is nil (i.e. IntelliJ reading project.clj) then return an empty version.
-       "_"
-       (if (and (not ahead?) (not dirty?))
-         tag
-         (let [[_ major minor patch suffix] (re-find #"v?(\d+)\.(\d+)\.(\d+)(-.+)?" tag)]
-           (if (nil? major)
-             ;; If tag is poorly formatted then return GIT-TAG-INVALID
-             "GIT-TAG-INVALID"
-             (let [patch' (try (Long/parseLong patch) (catch Throwable _ 0))
-                   patch+ (inc patch')]
-               (str major "." minor "." patch+ suffix "-" ahead "-SNAPSHOT")))))))}
+  :plugins      [[day8/lein-git-inject "0.0.2"]
+                 [lein-shadow          "0.1.6"]]
 
-  :plugins [[me.arrdem/lein-git-version "2.0.3"]]
+  :middleware   [leiningen.git-inject/middleware]
 
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
