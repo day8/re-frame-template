ed7b7e129fc7d0671d2983e4d1ec20bb65c15cb5
Steve Padilla <stevejmp-dev@fiftycm.com>
2019-12-21 21:24:26 +0900

    fix: completely remove runner.cljs from test option
    Fixes #104

--- src/leiningen/new/options/test.clj
+++ src/leiningen/new/options/test.clj
@@ -4,5 +4,4 @@
 (def option "+test")
 
 (defn files [data]
-  [["test/cljs/{{sanitized}}/runner.cljs" (helpers/render "test/cljs/runner.cljs" data)]
-   ["test/cljs/{{sanitized}}/core_test.cljs" (helpers/render "test/cljs/core_test.cljs" data)]])
+  [["test/cljs/{{sanitized}}/core_test.cljs" (helpers/render "test/cljs/core_test.cljs" data)]])
