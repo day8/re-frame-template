2ca3a220e2c408deb427a651ef8acb13b09c2eb5
Steve Padilla <stevejmp-dev@fiftycm.com>
2019-12-31 20:13:52 +0900

    refactor: remove unused requires, rename unused binding

--- src/leiningen/new/re_frame/src/clj/server.clj
+++ src/leiningen/new/re_frame/src/clj/server.clj
@@ -4,6 +4,6 @@
             [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
- (defn -main [& args]
+ (defn -main [& _args]
    (let [port (Integer/parseInt (or (env :port) "3000"))]
      (run-jetty handler {:port port :join? false})))
