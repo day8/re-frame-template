4737d68a4a5bc3b8165600df5128fe077d5b9cf3
Isaac Johnston <isaac@superstruct.nz>
2020-06-29 21:41:38 +1200

    Upgrade shadow-cljs to 2.10.13

--- src/leiningen/new/re_frame/project.clj
+++ src/leiningen/new/re_frame/project.clj
@@ -4,7 +4,7 @@
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs "2.9.3"]
+                 [thheller/shadow-cljs "2.10.13"]
                  [reagent "0.10.0"]
                  [re-frame "0.12.0"]{{#10x?}}
                  [day8.re-frame/tracing "0.5.6"]{{/10x?}}{{#re-com?}}
