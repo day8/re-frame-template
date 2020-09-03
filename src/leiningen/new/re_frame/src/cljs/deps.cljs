f012df87ff38015509829645c103f217215e35da
Isaac Johnston <isaac@superstruct.nz>
2020-09-04 00:48:38 +1200

    Upgrade karma to 5.2.1

--- src/leiningen/new/re_frame/src/cljs/deps.cljs
+++ src/leiningen/new/re_frame/src/cljs/deps.cljs
@@ -1,5 +1,5 @@
 {:npm-dev-deps {"shadow-cljs"           "2.11.0"{{#test?}}
-                "karma"                 "5.1.1"
+                "karma"                 "5.2.1"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"{{/test?}}}}
